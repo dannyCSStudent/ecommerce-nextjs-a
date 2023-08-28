51c4beffb8ce2b4a1c401603ea779abec9e7eead
Dan Villegas <danv8605@gmail.com>
2023-08-27 22:05:59 -0600

    Completed HeroBanner

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { FooterBanner, HeroBanner, Product } from '../components'
-
-const Home = () => {
+import { client } from '../lib/client'
+const Home = ({products, bannerData}) => {
+  console.log('sas ', products)
   return (
     <>
-        <HeroBanner />
+        <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
         <div className='products-heading'>
             <h2>Best Selling Products</h2>
@@ -12,11 +13,21 @@ const Home = () => {
         </div>
 
         <div className='products-container'>
-            {['Product 1', 'Product 2'].map((product) => product)}
+            {products?.map((product) => product.name)}
         </div>
         <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
